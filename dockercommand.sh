44c8dd8efa032abc75b65633143b7ae72ba3b88e
Gopi-Kishan <gopikishan.jakkam@analytiqs.io>
2022-09-12 12:13:05 +0530

    Dockerised the application

--- dockercommand.sh
+++ dockercommand.sh
@@ -1,6 +1,7 @@
-docker run -it --name data-copier --rm -v C:\Users\FL_LPT-320\Research\data\retail_db_json:/retail_db_json -e BASE_DIR=/retail_db_json -e DB_HOST=35f5ba1886cc -e DB_NAME=retail_db -e DB_PORT=5432 -e DB_USER=retail_user -e DB_PASS=itversity data-copier
+docker run -it --name data-copier --rm --network data-copier-nw -v C:\Users\FL_LPT-320\Research\data\retail_db_json:/retail_db_json -e BASE_DIR=/retail_db_json -e DB_HOST=35f5ba1886cc -e DB_NAME=retail_db -e DB_PORT=5432 -e DB_USER=retail_user -e DB_PASS=itversity data-copier
 
 docker run -it --name data-copier --rm \
+  --network data-copier-nw \
   -v C:\Users\FL_LPT-320\Research\data\retail_db_json:/retail_db_json \
   -e BASE_DIR=/retail_db_json \
   -e DB_HOST=35f5ba1886cc \
